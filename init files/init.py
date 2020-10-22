9eb932bee57be21a0838d3d6621722d7825474d0
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-10-22 16:26:06 -0700

    Started init files

---
+++ init files/init.py
@@ -0,0 +1,16 @@
+## This file will run every time you open the program
+
+print(
+    '''
+    =============================================================
+    |                                                           | 
+    |                                                           |
+    |                                                           |
+    |       Hello! Welcome to the Python Terminal Logger!       |
+    |        Please choose one of below to get started:         |
+    |                                                           |
+    |                                                           |
+    |                                                           |
+    =============================================================
+    '''
+)
---
+++ init files/init_options.py
@@ -0,0 +1,11 @@
+# This file opens up after init.py, giving user options
+
+print(
+    '''
+    A) Sign in as a current user
+    B) Make a user profile
+    C) Write a public log
+    D) View written logs
+    E) Exit the program
+    '''
+)
